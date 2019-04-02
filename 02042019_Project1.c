5c006097f3f64d938c08f331a32d2a5cab69f69c
funkygen <46698033+funkygen@users.noreply.github.com>
2019-04-02 13:22:43 +1100

    Rough start

--- 02042019_Project1.c
+++ 02042019_Project1.c
@@ -1,4 +1,36 @@
+/*ENNG1003 Project 1 - English Text Ciphers 
+
+			USER INTERFACE NOTES
+How to choose between encryption and decryption  -
+
+*/
+
+
+int rotCipherEnc (int x);		//Function prototype
+								//
+
 #include <stdio.h>
+
 int main() {
-    printf("Yay! Ciphers!");
+	
+	int a=0, b=1, c=2,d=3, e=4, f=5, g=6, h=7, i=8, j=9, k=10, l=11, m=12;
+	int n=13, o=14, p=15, q=16, r=17, s=18, t=19, u=20, v=21, w=22, x=23, y=24, z=25; 
+	int x[26] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,0}; 
+	
+    
+	
+	return 0;
 }
+
+								/*eg. BEFORE EVERY FUNCTION DEFINITION - 
+								Function block comment to document how the function works. 
+								Also it's inputs, return value and limitations*/
+	
+	/* ROTATION CIPHER ENCRIPTION FUNCTION - 
+	HOW IT WORKS:
+	INPUT: Letter of the Alphabet 
+	RETURN VALUE: Third letter of the Alphabet after the input letter.
+	LIMITATIONS:
+	*/
+	int rotCipherEnc (int x);	//Rotation cipher encription definition
+	
